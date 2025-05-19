414f79b003b98d5e96d70d801c1edd7aacf8da66
Thomas Sanger <thomas.sanger92@gmail.com>
2025-05-18 22:14:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilmingtonVetClinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print welcome message
+            Console.WriteLine("Welcome to the Wilmington Veterinary Clinic!\n");
+
+            // Print terminate message
+            Console.WriteLine("Press Enter twice to terminate...");
+            Console.Read();
+
+        }
+    }
+}
