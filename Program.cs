287a6d9331986c59d271acad0f8a6b5b0e5cfcda
Thomas Sanger <thomas.sanger92@gmail.com>
2025-06-04 15:09:06 -0600

    Completed up to week 5. Need to add logic to diplay certain pets based on pet types within while loop.

--- Program.cs
+++ Program.cs
@@ -60,21 +60,101 @@ namespace WilmingtonVetClinic
             Console.WriteLine("Number of pets: " + pets);
             Console.WriteLine("Total Household Administrative Fee: {0}", currentAdminFee.ToString("C", CultureInfo.GetCultureInfo("en-US")) + "\n");
 
+            // Dictionary for pet codes
+            Dictionary<char, string> petCodes = new Dictionary<char, string>
+            {
+                {'B', "Bird"},
+                {'C', "Cat"},
+                {'D', "Dog"},
+                {'R', "Reptile"}
+            };
+
+            // List for pet names
+            List<string> petNames = new List<string>();
+
+            // List for pet type codes
+            List<char> petTypes = new List<char>();
+
+            // List for pet ages
+            List<int> petAges = new List<int>();
+
+
             // Prompt for pet names
             int p = 0;
             while (true)
             {
                 Console.Write("Enter your pet's name: ");
-                Console.ReadLine();
+                string nameInput = Console.ReadLine();
+                petNames.Add(nameInput);
 
+                // Show pet codes
+                Console.WriteLine("The following pet type codes are available: ");
+                foreach(KeyValuePair<char, string> code in petCodes)
+                {
+                    Console.WriteLine("\t" + code.Key + "\t" + code.Value);
+                }
+
+                // Prompt for pet code
+                Console.Write($"Enter {nameInput}'s pet type code: ");
+                char codeInput = Convert.ToChar(Console.ReadLine().ToUpper());
+                petTypes.Add(codeInput);
+
+                // Prompt for pet age
+                Console.Write($"Enter {nameInput}'s age: ");
+                int ageInput = Convert.ToInt32(Console.ReadLine());
+                petAges.Add(ageInput);
+
+                // Insert break for readability
+                Console.WriteLine();
+
+                // Increment counter
                 p++;
 
+                // if counter is greater than or equal to pets, break out of while loop
                 if (p >= int.Parse(pets))
                 {
                     break;
                 }
 
-            }            
+            }
+
+            bool quit = false;
+            while(quit != true) 
+            {
+                // Prompt for pet code to display pets of that type
+                Console.WriteLine("Enter pet type to see listings, or 'Q' to Quit");
+                foreach(KeyValuePair<char, string> key in petCodes)
+                {
+                    Console.WriteLine("\t" + key.Key + "\t" + key.Value);
+                }
+
+                char input = Convert.ToChar(Console.ReadLine().ToUpper());
+
+                if(input == 'Q')
+                {
+                    break;
+                }
+                else if (input == 'B')
+                {
+                    Console.WriteLine("The following pets are of type Bird: ");
+                }
+                else if (input == 'C')
+                {
+                    Console.WriteLine("The following pets are of type Cat: ");
+                }
+                else if (input == 'D')
+                {
+                    Console.WriteLine("The following pets are of type Dog: ");
+                }
+                else if (input == 'R')
+                {
+                    Console.WriteLine("The following pets are of type Reptile: ");
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid response!");
+                }
+            }
 
             // Print terminate message
             Console.WriteLine("\nPress Enter twice to terminate...");
